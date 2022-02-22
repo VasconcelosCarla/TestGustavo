b2d869288d4e96d1bba2ae400da6c7afb5a361b4
Carla Vasconcelos <88123492+VasconcelosCarla@users.noreply.github.com>
2022-02-22 18:57:02 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var bola, raquete, coelho;
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  bola = createSprites(200, 200, 20, 20);
+  
+
+}
+
+function draw() {
+  background(220);
+  drawSprites();
+}
